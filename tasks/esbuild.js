ca5e3efc0f9a8679e7bc4d205444783d27de42d7
Kevin Lint <kevin@kevinlint.com>
2023-01-06 10:21:09 -0800

    prep

--- tasks/esbuild.js
+++ tasks/esbuild.js
@@ -1,14 +1,17 @@
 const esbuild = require("esbuild");
 const env = process.env.NODE_ENV || "development";
+const pkg = require("../package.json");
 
 esbuild.build({
   bundle: true,
+  entryNames: `element`,
   entryPoints: ["src/index.ts"],
   globalName: "OEM.Elements",
   minify: true,
   outdir: "lib",
   sourcemap: true,
   target: ["esnext"],
+  treeShaking: true,
   watch: env === "development",
   write: true,
 });
